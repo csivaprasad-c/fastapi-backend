902da2362bb87d9758fa5007f422db3f5af229a0
Sivaprasad Chidambaram <csivaprasad.c@icloud.com>
2026-04-05 21:49:05 +0530

    context manager

--- app/database.py
+++ app/database.py
@@ -1,14 +1,17 @@
 # import json
 import sqlite3
 from typing import Any
+from contextlib import contextmanager
 
 from app.schemas import ShipmentCreate, ShipmentPatch, ShipmentStatus, ShipmentUpdate
 
 class Database:
-    def __init__(self):
+    # def __init__(self):
+    #     self.create_table("shipments")
+
+    def connect_to_db(self):
         self.connection = sqlite3.connect("shipments.db", check_same_thread=False)
         self.cursor = self.connection.cursor()
-        self.create_table("shipments")
 
     def create_table(self, name: str):
         self.cursor.execute(f"""
@@ -101,7 +104,29 @@ class Database:
         """, (shipment_id,))
         self.connection.commit()
 
+    def close(self):
+        self.connection.close()
+    
+    def __enter__(self):
+        self.connect_to_db()
+        self.create_table("shipments")
+        return self
+    
+    def __exit__(self, *arg):
+        self.close()
+        
+@contextmanager
+def managed_db():
+    db = Database()
+    db.connect_to_db()
+    db.create_table("shipments")
 
+    yield db
+
+    db.close()
+
+with managed_db() as db:
+    print(db.get(2))
 
 # shipments = {}
 
