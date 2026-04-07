4bec5d3e1cc06b8d3383a62463bcff8952ef1f1a
Sivaprasad Chidambaram <csivaprasad.c@icloud.com>
2026-04-07 17:55:28 +0530

    HTTPBearer

--- app/core/security.py
+++ app/core/security.py
@@ -1,3 +1,29 @@
-from fastapi.security import OAuth2PasswordBearer
+from typing import Annotated
+
+from fastapi import HTTPException, Depends
+from fastapi.security import HTTPBearer, OAuth2PasswordBearer
+
+from app.utils import decode_token
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/sellers/token")
+
+class AccessTokenBearer(HTTPBearer):
+    async def __call__(self, request):
+        auth_credentials = await super().__call__(request)
+        token = auth_credentials.credentials
+
+        token_data = decode_token(token)
+
+        if token_data is None:
+            raise HTTPException(
+                status_code=401,
+                detail="Invalid authentication credentials",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        
+        return token_data
+    
+access_token_bearer = AccessTokenBearer()
+
+Annotated[dict, Depends(access_token_bearer)]
+
